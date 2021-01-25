7594216cc7bd80ba2fbf7a5c209c32cd08ff69bb
deborah-p <deborah.powers@cgi.com>
2021-01-25 18:46:00 +0100

    boulot 01/25

--- tmp.py
+++ tmp.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
-from logger import Logger
+from fileHtml import FileHtml
+import urllib as ul
+from urllib import request as urlRequest
 
-log = Logger()
-log.log ('a')
-log.coucou()
+urlF = 'https://mantis2.axyus.com/view.php?id=660'
+urlD = 'https://mantis2.axyus.com/login_password_page.php'
+urlE = 'https://mantis2.axyus.com/login.php'
+params ={
+	'username': 'deborah.powers',
+	'password': 'DePoS2020**'
+}
 
-class Test():
-	def __init__ (self):
-		self.a = 'a'
-		log.log ('b')
+def fillForm (url):
+	paramsUrl = ul.parse.urlencode (params).encode ('utf-8')
+	myRequest = urlRequest.Request (url, method='POST')
+	response = urlRequest.urlopen (myRequest, paramsUrl)
+"""
+fillForm (urlD)
+fillForm (urlE)
+"""
+file = FileHtml()
+file.link = urlE
+file.fromUrlVa (params)
+file.title = 'tmp'
+file.fileFromData()
+file.toFile()
 
-	def printC (self):
-		log.log ('c')
-
-test = Test()
-test.printC()
