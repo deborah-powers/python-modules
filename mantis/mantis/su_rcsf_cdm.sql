48b3c0ac6043279bbbcd4d996e6b428dcf33bd76
deborah-p <deborah.powers@cgi.com>
2021-05-21 17:00:30 +0200

    boulot 05/21 17:00

--- mantis/mantis/su_rcsf_cdm.sql
+++ mantis/mantis/su_rcsf_cdm.sql
@@ -1,5 +1,5 @@
 ﻿----------------------------------------------------------------------------------------------
--- Script d'exemple
+-- rétrogradage du CSF1 de OP0000000
 -- Source : cf. Mantis %numero%
 -- Date : %date%
 -- Auteur : CGI
@@ -8,10 +8,16 @@
 set schema 'cdm';
 
 do $$ begin
+	delete from suivi_integration_flux where suvi_inte_flx_typ_msg = 'FLUX_F3' and suvi_inte_flx_id_obj = 00000;
+	update csf_versionne set
+		motivation_ac = null, etat_precedent = null, date_decision_valnc = null, commentaire_decision_valnc = null,
+		etat = 'EAVAC' where csf_versionne_pk = 00000;
 
 	-- Mise à jour des flags de transfert vers l'agrégateur. quand je modifie une opération ou un objet qui en dépend
-	update operation set statut_flux_agr = 1, statut_flux_agr_f3 = 1 where id_fonctionnel = 'MP0002386';
+	update operation set statut_flux_agr = 1, statut_flux_agr_f3 = 1 where id_fonctionnel = 'OP0000000';
 	-- tracer l'action
 	insert into suivi_execution_script 
-	values (nextval('suivi_execution_script_ses_pk_seq'), 'CDM', '%numero%', 'su_%numero%_', now(), 'Script d''exemple', '' , false);
+	values (nextval('suivi_execution_script_ses_pk_seq'), 'CDM', '%numero%', 'su_%numero%_retrogradage_csf', now(),
+		'rétrogradage du CSF1 de OP0000000',
+		'OP0000000-CSF1-V1: 00000' , false);
 end $$
