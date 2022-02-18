a73a904724aafc60ca724ab2e00a2d03f3713803
deborah-powers <deborah.powers.dev@gmail.com>
2022-02-18 17:43:49 +0100

    boulot 02/18 17:43

---
+++ mapperFouille.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+from fileSimple import File
+
+mapperPath = ''
+xsdPath = ''
+boPath = ''
+
+mapperFile = File (mapperPath)
+xsdFile = File (xsdPath)
+boFile = File (boPath)
+
+mapperFile.fromFile()
+
+
+
+
+
+
+
+
+
