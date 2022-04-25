abe1dcebd19824d3bb1234cfed5a252e98f74433
deborah-powers <deborah.powers.dev@gmail.com>
2022-04-25 17:25:25 +0200

    boulot 04/25 17:25

--- cleanSql.py
+++ cleanSql.py
@@ -8,24 +8,41 @@ import funcLogger
 sql = File ('b/requete.txt')
 sql.read()
 # nettoyer le texte
-sql.text = sql.text.replace ('\n', "")
+sql.text = sql.text.replace ('\n', ' ')
 sql.text = sql.text.replace ('\t', ' ')
 sql.text = funcText.clean (sql.text)
 sql.text = funcText.upperCase (sql.text, 'reset')
+sql.text = sql.text.replace ('. ', '.')
 
-wordSql = ('where')
-wordSqlSpace = ('inner join', 'left join')
+wordsSql = ('from', 'where')
+wordsSqlSpace = ('inner join', 'left join', 'group by')
+wordsSqlInner = (('eq', '='), ('in', 'in'))
+
+d= sql.text.find ('(q_')
+sql.text = sql.text[d:]
 
 sql.text = sql.text.replace (';', "")
 sql.text = sql.text.replace ('_dpo', "")
 sql.text = sql.text.replace ('(q_', '(')
-sql.text = sql.text.replace (') . where (', '\nwhere ')
-sql.text = sql.text.replace (') . where (', '\nwhere ')
+sql.text = sql.text.replace (' q_', ' ')
+for word in wordsSqlSpace: sql.text = sql.text.replace (').'+ word.replace (" ","") +' (', '\n'+ word +' ')
+for word, char in wordsSqlInner: sql.text = sql.text.replace ('.'+ word +' (', ' '+ char +' ')
+for word in wordsSql: sql.text = sql.text.replace (').'+ word +' (', '\n'+ word +' ')
+sql.text = sql.text.strip ('()')
 
-funcLogger.log (sql.text)
+# les jointures
+sqlList = sql.text.split ('\n')
+sqlRange = funcList.range (sqlList)
+for s in sqlRange:
+	if ' join ' in sqlList[s]:
+		d= sqlList[s].find (' join ') +6
+		e= sqlList[s].find ('.')
+		f= sqlList[s].find (', ')
+		sqlList[s] = sqlList[s][:d] + sqlList[s][f+2:] +' on '+ sqlList[s][f+2:] + '.pk = '+ sqlList[s][d:e] + '.pk'
+sql.text = '\n'.join (sqlList)
 
-d= sql.text.find ('(q_')
-sql.text = sql.text[d:]
+sql.text = 'select *\nfrom '+ sql.text
+print (sql.text)
 
 
 
