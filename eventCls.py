11cd49011edf8661ea2504a10c5886422bc93ca6
deborah-powers <deborah.powers.dev@gmail.com>
2023-08-27 17:44:27 +0200

    maison 08/27 17:44

--- eventCls.py
+++ eventCls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 import loggerFct
 
 help ="""
@@ -34,6 +34,12 @@ class DatePerso (datetime):
 	def __init__ (self, year, month, day, hour=0, minute=0):
 		datetime.__init__(self)
 		self.monthName = monthNames[self.month -1]
+
+	def difference (self, newDate):
+		delta = None
+		if self < newDate: delta = newDate - self
+		else: delta = self - newDate
+		return delta
 
 	def today():
 		dtDate = datetime.now()
@@ -74,6 +80,10 @@ DatePerso.fromStr = staticmethod (DatePerso.fromStr)
 DatePerso.fromUtz = staticmethod (DatePerso.fromUtz)
 DatePerso.fromGoogle = staticmethod (DatePerso.fromGoogle)
 
+data = DatePerso.fromGoogle ('2018-12-22T12:45')
+dato = DatePerso.fromGoogle ('2018-12-14T12:26')
+data.difference (dato)
+
 """
 dt= DatePerso.today()
 print (dt)
@@ -86,70 +96,6 @@ print (do.hour)
 print (do.minute)
 print (do.replace (day=28, hour=4))
 """
-class DatePersoVa():
-	def difference (self, newDate):
-		diff = self.compare (newDate)
-		nbMinutesA =0
-		nbMinutesB =0
-		if diff >0:
-			nbMinutesA = self.toMinutes()
-			nbMinutesB = newDate.toMinutes()
-		else:
-			nbMinutesA = newDate.toMinutes()
-			nbMinutesB = self.toMinutes()
-		nbMinutesA -= nbMinutesB
-		diffDate = DatePerso()
-		diffDate.fromMinutes (nbMinutesA)
-		return diffDate
-
-	def __lt__ (self, newDate):
-		return self.__str__() > newDate.__str__()
-
-	def compare (self, newDate):
-		diff =0
-		if self.year		> newDate.year:	diff =5
-		elif self.year		< newDate.year:	diff =-5
-		elif self.month		> newDate.month:		diff =4
-		elif self.month		< newDate.month:		diff =-4
-		elif self.day		> newDate.day:		diff =3
-		elif self.day		< newDate.day:		diff =-3
-		elif self.hour		> newDate.hour:	diff =2
-		elif self.hour		< newDate.hour:	diff =-2
-		elif self.minute	> newDate.minute:	diff =1
-		elif self.minute	< newDate.minute:	diff =-1
-		return diff
-
-	def toDays (self):
-		# number of days since year 0
-		numberDays = 365 * self.year
-		nbYearsBissextiles = self.year //4 + self.year // 400 - self.year // 100
-		numberDays += nbYearsBissextiles
-		numberDays += self.nbDaysYear()
-
-	def toMinutes (self):
-		return self.minute + 60* self.hour + 1440 * self.toDays()
-
-	def fromDays (self, nbDays):
-		while (nbDays > 364):
-			self.year +=1
-			nbDays -= 365
-			if self.isBissextile(): nbDays -=1
-		m=0
-		while nbDays > monthDuration[m]:
-			nbDays -= monthDuration[m]
-			m+=1
-		if m<2 and self.isBissextile(): nbDays +=1
-		self.month = m+1
-		self.day = nbDays
-		self.monthName = monthNames[month -1]
-
-	def fromMinutes (self, nbMinutes):
-		nbDays = nbMinutes // 1440
-		self.fromDays (nbDays)
-		nbMinutes = nbMinutes % 1440
-		self.hour = nbMinutes // 60
-		self.minute = nbMinutes % 60
-
 
 class Event():
 	def __init__ (self):
@@ -169,6 +115,12 @@ class Event():
 
 	def __str__ (self):
 		return self.toStrDay()
+
+	def setDuration (self, datePerso):
+		if datePerso < self.date: self.duration =0
+		else:
+			difference = self.date.difference (datePerso)
+			if difference.days ==0 and difference.seconds >0: self.duration = difference.seconds /60
 
 	def toStrHour (self):
 		strEvt = self.date.toStrHour() +'\t'+ self.category +'\t'+ self.title +'\t'+ self.location +'\n\t'+ self.infos
