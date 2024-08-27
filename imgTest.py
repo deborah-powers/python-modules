cf87e312ef5032064b6ad68f42583e702099d89e
deborah-powers <deborah.powers.dev@gmail.com>
2024-08-27 21:47:25 +0200

    maison 08/27 21:47

--- imgTest.py
+++ imgTest.py
@@ -23,12 +23,12 @@ def findBorder (imageName):
 	# simplifier une image en vue de détecter sa bordure
 	newName, imageOriginal = openImage (imageName)
 	newName = newName + '-simple.bmp'
-	imageLd = imageOriginal.convert ('P', palette=Image.ADAPTIVE, colors=4)
+	imageLd = imageOriginal.convert ('P', palette=Image.ADAPTIVE, colors=3)
 	imageLd = ImageOps.grayscale (imageLd)
 	colors = getColors (imageLd)
 	imageArray = numpy.array (imageLd)
 	# unir les couleurs proches
-	rangeColors = (3,2,1)
+	rangeColors = (2,1)
 	for c in rangeColors:
 		score = int (colors[c]) - int (colors[c-1])
 		if score >= -10 and score <= 10:
@@ -66,14 +66,51 @@ def findBorder (imageName):
 	imageArray[colorArea] =0
 	colorArea = widthArray ==0
 	imageArray[colorArea] =0
-	"""
-	colorArea = imageArray !=0
+	# effacer la couleur du centre
+	halfHeight = int (lenHeight /2)
+	halfWidth = int (lenWidth /2)
+	# lignes horizontales
+	heightArray = imageArray.copy()
+	for h in rangeHeight:
+		if imageArray[h][halfWidth] !=0:
+			w= halfWidth
+			while w< lenWidth and imageArray[h][w] == imageArray[h][halfWidth]:
+				heightArray[h][w] =255
+				w+=1
+			w= halfWidth
+			while w>=0 and imageArray[h][w] == imageArray[h][halfWidth]:
+				heightArray[h][w] =255
+				w-=1
+	# lignes verticales
+	widthArray = imageArray.copy()
+	for w in rangeWidth:
+		if imageArray[halfHeight][w] !=0:
+			h= halfHeight
+			while h< lenHeight and imageArray[h][w] == imageArray[halfHeight][w]:
+				heightArray[h][w] =255
+				h+=1
+			h= halfWidth
+			while h>=0 and imageArray[h][w] == imageArray[halfHeight][w]:
+				heightArray[h][w] =255
+				h-=1
+	colorArea = heightArray ==255
 	imageArray[colorArea] =255
-	""
+	colorArea = widthArray ==255
+	imageArray[colorArea] =255
+	colorArea = numpy.logical_and (imageArray !=255, imageArray !=0)
+	imageArray[colorArea] =130
+	# effacer les boucles blanches
+	print (numpy.where (imageArray==255))
+	# https://stackoverflow.com/questions/66930670/attributeerror-numpy-ndarray-object-has-no-attribute-index
+
+	# dessiner la nouvelle image
+	imageNouvelle = Image.fromarray (imageArray)
+	imageNouvelle.save (newName)
+
+def findBorder_fin (newName, imageArray):
 	# remplir les boucles blanches incluses dans du noir
 	halfHeight = int (lenHeight /2)
 	halfWidth = int (lenWidth /2)
-	""
 	for h in rangeHeight:
 		w= halfWidth
 		while w>=0 and imageArray[h][w] !=0: w-=1
@@ -96,7 +133,8 @@ def findBorder (imageName):
 		while h< lenHeight:
 			if imageArray[h][w] !=0: imageArray[h][w] =255
 			h+=1
-	"""
+	colorArea = imageArray !=0
+	imageArray[colorArea] =255
 	# dessiner la nouvelle image
 	imageNouvelle = Image.fromarray (imageArray)
 	imageNouvelle.save (newName)
