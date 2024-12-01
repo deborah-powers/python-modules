99237a7a0f13e8af0c2164de413f0933f3a421bb
deborah-powers <deborah.powers.dev@gmail.com>
2024-12-01 16:59:04 +0100

    maison 12/01 16:59

--- insta va.py
+++ insta va.py
@@ -6,7 +6,6 @@ from PIL import Image, ImageDraw
 import cv2
 import fileLocal
 from imgModif import openImage
-import loggerFct as log
 """
 créer un gif animé carré afin de mettre une grande image sur instagram
 
@@ -78,33 +77,31 @@ def squarePictureComputeColors (color, pixel):
 	color[2] += pixel[2]
 	return color
 
-def drawBgStripes (imageArray, width, height, horizontal=True):
+def drawBgStripes (imageArray, length, horizontal=True):
+	lengthalf = int (length /2)
 	# dessiner la nouvelle image
-	imageObj = Image.new (mode='RGB', size=(width, height), color=(0,0,0))
+	imageObj = Image.new (mode='RGB', size=(length, length), color=(0,0,0))
 	drawing = ImageDraw.Draw (imageObj)
+	rangeBord = range (length)
 	# la largeur est plus grande que la hauteur
 	if horizontal:
-		rangeBord = range (width)
-		lengthalf = int (height /2)
 		for p in rangeBord:
 			drawing.rectangle (((p, 0), (p+1, lengthalf)), fill=( imageArray[0][p][0], imageArray[0][p][1], imageArray[0][p][2] ))
-			drawing.rectangle (((p, lengthalf), (p+1, height)), fill=( imageArray[-1][p][0], imageArray[-1][p][1], imageArray[-1][p][2] ))
+			drawing.rectangle (((p, lengthalf), (p+1, length)), fill=( imageArray[-1][p][0], imageArray[-1][p][1], imageArray[-1][p][2] ))
 	# la hauteur est plus grande que la largeur
 	else:
-		rangeBord = range (height)
-		lengthalf = int (width /2)
 		for p in rangeBord:
 			drawing.rectangle (((0, p), (lengthalf, p+1)), fill=( imageArray[p][0][0], imageArray[p][0][1], imageArray[p][0][2] ))
-			drawing.rectangle (((lengthalf, p), (width, p+1)), fill=( imageArray[p][-1][0], imageArray[p][-1][1], imageArray[p][-1][2] ))
+			drawing.rectangle (((lengthalf, p), (length, p+1)), fill=( imageArray[p][-1][0], imageArray[p][-1][1], imageArray[p][-1][2] ))
 	drawing = ImageDraw.Draw (imageObj)
 	return imageObj
 
-def drawBgMix (imageArray, width, height, horizontal=True):
+def drawBgColorMixed (imageArray, length, horizontal=True):
 	# calculer la couleur moyenne de chaque coté
 	colorA =[ 0,0,0 ]
 	colorB =[ 0,0,0 ]
+	rangeBord = range (length)
 	if horizontal:
-		rangeBord = range (width)
 		for i in rangeBord:
 			colorA[0] += imageArray[0][i][0]
 			colorA[1] += imageArray[0][i][1]
@@ -112,14 +109,7 @@ def drawBgMix (imageArray, width, height, horizontal=True):
 			colorB[0] += imageArray[-1][i][0]
 			colorB[1] += imageArray[-1][i][1]
 			colorB[2] += imageArray[-1][i][2]
-		colorA[0] = int (colorA[0] / width)
-		colorA[1] = int (colorA[1] / width)
-		colorA[2] = int (colorA[2] / width)
-		colorB[0] = int (colorB[0] / width)
-		colorB[1] = int (colorB[1] / width)
-		colorB[2] = int (colorB[2] / width)
 	else:
-		rangeBord = range (height)
 		for i in rangeBord:
 			colorA[0] += imageArray[i][0][0]
 			colorA[1] += imageArray[i][0][1]
@@ -127,43 +117,36 @@ def drawBgMix (imageArray, width, height, horizontal=True):
 			colorB[0] += imageArray[i][-1][0]
 			colorB[1] += imageArray[i][-1][1]
 			colorB[2] += imageArray[i][-1][2]
-		colorA[0] = int (colorA[0] / height)
-		colorA[1] = int (colorA[1] / height)
-		colorA[2] = int (colorA[2] / height)
-		colorB[0] = int (colorB[0] / height)
-		colorB[1] = int (colorB[1] / height)
-		colorB[2] = int (colorB[2] / height)
+	colorA[0] = int (colorA[0] / length)
+	colorA[1] = int (colorA[1] / length)
+	colorA[2] = int (colorA[2] / length)
+	colorB[0] = int (colorB[0] / length)
+	colorB[1] = int (colorB[1] / length)
+	colorB[2] = int (colorB[2] / length)
+	lengthalf = int (length /2)
 	# dessiner la nouvelle image
-	imageObj = Image.new (mode='RGB', size=(width, height), color=(colorB[0], colorB[1], colorB[2]))
+	imageObj = Image.new (mode='RGB', size=(length, length), color=(colorB[0], colorB[1], colorB[2]))
 	drawing = ImageDraw.Draw (imageObj)
 	# la largeur est plus grande que la hauteur
-	if horizontal:
-		lengthalf = int (height /2)
-		drawing.rectangle (((0,0), (width, lengthalf)), fill=( colorA[0], colorA[1], colorA[2] ))
+	if horizontal: drawing.rectangle (((0,0), (length, lengthalf)), fill=( colorA[0], colorA[1], colorA[2] ))
 	# la hauteur est plus grande que la largeur
-	else:
-		lengthalf = int (width /2)
-		drawing.rectangle (((0,0), (lengthalf, height)), fill=( colorA[0], colorA[1], colorA[2] ))
+	else: drawing.rectangle (((0,0), (lengthalf, length)), fill=( colorA[0], colorA[1], colorA[2] ))
 	drawing = ImageDraw.Draw (imageObj)
 	return imageObj
 
-def squarePicture (nomCarre, width, height, imageOriginal, drawBgfonc, ratio):
-	nomCarre = nomCarre +'-rectan.jpg'
+def squarePicture (nomCarre, width, height, imageOriginal, drawBgfonc):
+	nomCarre = nomCarre +'-carre.jpg'
 	imageArray = numpy.array (imageOriginal)	# imageArray is a height x width x 4 numpy array
 	# red, green, blue = imageArray.T	# Temporarily unpack the bands for readability
 	# la largeur est plus grande que la hauteur
 	if width > height:
-		heightTmp = ratio * width
-		heightNew = int (heightTmp)
-		imageObj = drawBgfonc (imageArray, width, heightNew, True)
-		wStripe = int ((heightNew - height) /2)
+		imageObj = drawBgfonc (imageArray, width, True)
+		wStripe = int ((width - height) /2)
 		imageObj.paste (imageOriginal, (0, wStripe))
 	# la hauteur est plus grande que la largeur
 	else:
-		widthTmp = ratio * height
-		widthNew = int (widthTmp)
-		imageObj = drawBgfonc (imageArray, widthNew, height, False)
-		wStripe = int ((widthNew - width) /2)
+		imageObj = drawBgfonc (imageArray, height, False)
+		wStripe = int ((height - width) /2)
 		imageObj.paste (imageOriginal, (wStripe, 0))
 	imageObj.save (nomCarre)
 
@@ -173,21 +156,12 @@ else:
 	nomCarre, imageOriginal = openImage (argv[1])
 	width = imageOriginal.size[0]
 	height = imageOriginal.size[1]
-	ratio =1
-	ratioB =1
-	if len (argv) >2: ratio = int (argv[2])
-	if len (argv) >3: ratioB = int (argv[3])
-	if ratio < ratioB: ratio = ratio / ratioB
-	else: ratio = ratioB / ratio
-	if width > height:
-		ratioB = height / width
-		if ratioB == ratio: print ("votre image est déjà au bon ratio, vous n'avez pas besoin de la transformer")
-		elif ratioB >= 0.9 * ratio: print ("votre image est presque au bon ratio, vous n'avez pas besoin de la transformer")
-		elif ratioB <0.3: squareVideo (nomCarre, width, height, imageOriginal)
-		else: squarePicture (nomCarre, width, height, imageOriginal, drawBgStripes, ratio)
-	else:
-		ratioB = width / height
-		if ratioB == ratio: print ("votre image est déjà au bon ratio, vous n'avez pas besoin de la transformer")
-		elif ratioB >= 0.9 * ratio: print ("votre image est presque au bon ratio, vous n'avez pas besoin de la transformer")
-		elif ratioB <0.3: squareVideo (nomCarre, width, height, imageOriginal)
-		else: squarePicture (nomCarre, width, height, imageOriginal, drawBgStripes, ratio)
+	# vérification sur ses dimensions
+	if width == height: print ("votre image est déjà un carré, vous n'avez pas besoin de la transformer")
+	elif width > height:
+		if height / width >=0.9: print ("votre image est presque carrée, vous n'avez pas besoin de la transformer")
+		elif height / width <0.5: squareVideo (nomCarre, width, height, imageOriginal)
+		else: squarePicture (nomCarre, width, height, imageOriginal, drawBgStripes)
+	elif width / height >=0.9: print ("votre image est presque carrée, vous n'avez pas besoin de la transformer")
+	elif width / height <0.5: squareVideo (nomCarre, width, height, imageOriginal)
+	else: squarePicture (nomCarre, width, height, imageOriginal, drawBgStripes)
