2d8337fcfd121c24e3c97903c171991636baf755
deborah-powers <deborah.powers.dev@gmail.com>
2025-06-10 19:18:34 +0200

    travail 06/10 19:18

--- convert-html-to-word.py
+++ convert-html-to-word.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import pypandoc
 
-filePdf = 'https://magellanpartners.sharepoint.com/:b:/r/sites/MP-PRJ-ANCTSYNERGIECLOUD/Documents%20partages/General/04.%20UO01%20Pilotage%20-%20Suivi%20contractuel/02.%20Bons%20de%20commandes/BDC%20E2024000286%20AXYUS_MCO%20n%C2%B01%2030S.pdf?csf=1&web=1&e=8Pbv58'
+filePdf = 'C:\\Users\\deborah.powers\\Desktop\\bdc.pdf'
 fileHtml = 'C:\\Users\\deborah.powers\\Desktop\\fichier.html'
 output = pypandoc.convert_file (filePdf, 'html', outputfile=fileHtml)
 
