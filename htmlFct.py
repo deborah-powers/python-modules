d320045e146a5473ff1ca71f75b1229ab01eb1b1
deborah-powers <deborah.powers.dev@gmail.com>
2025-07-28 21:57:55 +0200

    maison 07/28 21:57

--- htmlFct.py
+++ htmlFct.py
@@ -36,6 +36,7 @@ def imageFromBase64One (imgStr):
 	return Image.open (buff)
 
 def imgToB64One (imageName):
+	log.message (imageName)
 	imageName = imageName.replace ('c:', 'C:')
 	imageOriginal = Image.open (imageName)
 	imageOriginal = imageOriginal.convert ('RGB')
