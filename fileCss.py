09e8279da58e14756c22a7287c5ac4aa695f6f63
deborah-powers <deborah.powers.dev@gmail.com>
2026-05-22 17:25:50 +0200

    travail 05/22 17:25

--- fileCss.py
+++ fileCss.py
@@ -25,6 +25,89 @@ class RuleCss():
 		for rule in rules: res = res +'\t' + rule +": "+ self.rules[rule]
 		return res
 
+	def __lt__ (self, otherRule):
+		if self.query >= otherRule.query: return False
+		ruleSelf = self.rules.keys()
+		ruleOTher = otherRule.rules.keys()
+		nbRules = len (ruleSelf)
+		if nbRules >= len (ruleOTher): return False
+		r=0
+		isBefore = True
+		while isBefore and r< nbRules:
+			if ruleSelf[r] in ruleOTher and self.rules [ruleSelf[r]] >= ruleOTher.rules [ruleSelf[r]]: isBefore = False
+			r+=1
+		return isBefore
+
+	def __gt__ (self, otherRule):
+		if self.query <= otherRule.query: return False
+		ruleSelf = self.rules.keys()
+		ruleOTher = otherRule.rules.keys()
+		nbRules = len (ruleSelf)
+		if nbRules <= len (ruleOTher): return False
+		r=0
+		isAfter = True
+		while isAfter and r< nbRules:
+			if ruleSelf[r] in ruleOTher and self.rules [ruleSelf[r]] <= ruleOTher.rules [ruleSelf[r]]: isAfter = False
+			r+=1
+		return isAfter
+
+	def __le__ (self, otherRule):
+		return not self.__gt__ (otherRule)
+
+	def __ge__ (self, otherRule):
+		return not self.__lt__ (otherRule)
+
+	def __eq__ (self, otherRule):
+		if self.query != otherRule.query: return False
+		ruleSelf = self.rules.keys()
+		ruleOTher = otherRule.rules.keys()
+		r=0
+		nbRules = len (ruleSelf)
+		isSameRule = True
+		while isSameRule and r< nbRules:
+			if ruleSelf[r] not in ruleOTher: isSameRule = False
+			elif self.rules [ruleSelf[r]] != ruleOTher.rules [ruleSelf[r]]: isSameRule = False
+			r+=1
+		return isSameRule
+
+	def __ne__ (self, otherRule):
+		return not self.__eq__ (otherRule)
+
+	def sameStructure (self, otherRule):
+		if self.query != otherRule.query: return False
+		ruleSelf = self.rules.keys()
+		ruleOTher = otherRule.rules.keys()
+		r=0
+		nbRules = len (ruleSelf)
+		isSameRule = True
+		while isSameRule and r< nbRules:
+			if ruleSelf[r] not in ruleOTher: isSameRule = False
+			r+=1
+		if not isSameRule: return False
+		r=0
+		nbRules = len (ruleOTher)
+		while isSameRule and r< nbRules:
+			if ruleOTher[r] not in ruleSelf: isSameRule = False
+			r+=1
+		return isSameRule
+
+	def __contains__ (self, rule):
+		# vérifier si un élément est dans self. rule = "nomRule" ou { nomRule: valeurRule }
+		isIn = True
+		ruleKeysSelf = self.rules.keys()
+		if isinstance (rule, str):
+			if rule not in ruleKeysSelf: isIn = False
+			return isIn
+		elif isinstance (rule, dict):
+			ruleKeys = rule.rules.keys()
+			r=0
+			nbRules = len (ruleKeys)
+			while isIn and r< nbRules:
+				if ruleKeys[r] not in ruleKeysSelf: isIn = False
+				r+=1
+			return isIn
+		else: return False
+
 class QueryCss():
 	def __init__ (self):
 		self.query =""	# (max-width: 768px) ou print
@@ -48,6 +131,20 @@ class QueryCss():
 			res = res +'\n\t'+ rule.__str__()
 		return res
 
+	def __contains__ (self, rule):
+		if isinstance (rule, str):	# nom de règle, div, p.class-name
+			isIn = False
+			r=0
+			nbRules = len (self.rules)
+			while not isIn and r< nbRules:
+				if self.rules[r].__contains__ (rule): isIn = True
+				r+=1
+			return isIn
+		elif isinstance (rule, RuleCss):
+			if not rule in self.rules: isIn = False
+			return isIn
+		else: return False
+
 class FileCss (File):
 	def __init__ (self, file =None):
 		File.__init__ (self, file)
@@ -58,9 +155,20 @@ class FileCss (File):
 		rule = RuleCss()
 		rule.query = item
 		for ruleSelf in self.rules:
-			if item == ruleSelf.query or " "+ item +" " in ruleSelf.query or " "+ item +',' in ruleSelf.query or " "+ item +'>' in ruleSelf.query:
-				for line in ruleSelf.rules: rule.rules.append (line)
-		print (rule)
+			if item not in ruleSelf.query or item +" " in ruleSelf.query: continue
+			elif item +'.' in ruleSelf.query or item +'#' in ruleSelf.query or item +'[' in ruleSelf.query: continue
+			elif item +':' in ruleSelf.query or item +'>' in ruleSelf.query or item +'=' in ruleSelf.query: continue
+			elif ruleSelf.query == item or " "+ item +',' in ruleSelf.query or ruleSelf.query.find (item +',') ==0:
+				# si la même règle est répétée plusieurs fois, la dernière occurence est gardée
+				rulesNames = ruleSelf.rules.keys()
+				for name in rulesNames: rule.rules[name] = ruleSelf.rules[name]
+			elif ", "+ item in ruleSelf.query:
+				# item est le dernier élément de la query
+				pos = ruleSelf.query.rfind (", "+ item)
+				if len (ruleSelf.query) -pos -2 == len (item):
+					rulesNames = ruleSelf.rules.keys()
+					for name in rulesNames: rule.rules[name] = ruleSelf.rules[name]
+		return rule
 
 	def read (self):
 		File.read (self)
@@ -119,5 +227,6 @@ class FileCss (File):
 fileName = 's/library-css\\structure.css'
 fileCss = FileCss (fileName)
 fileCss.read()
-fileCss.getRulesForItem ('p')
+rule = fileCss.getRulesForItem ('p')
 
+print (isinstance (rule, RuleCss), isinstance ('hello', str), isinstance ({ 'a':2, 'b':4 }, dict))
