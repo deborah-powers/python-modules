ecd94c4f7d2c38105fdf3e4ddd9f90e24e93873c
deborah-powers <deborah.powers.dev@gmail.com>
2026-07-09 16:54:33 +0200

    travail 07/09 16:54

--- image.py
+++ image.py
@@ -13,27 +13,9 @@ les valeurs de tag
 	e imageRef: éffacer certaines couleurs de l'image principale. imageRef contient ces couleurs
 	s: simplifier les couleurs
 """
-imageName = 'C:\\Users\\deborah.powers\\Desktop\\photo-deborah.bmp'
 imageRefName = 'C:\\Users\\deborah.powers\\Desktop\\ref.bmp'
 
 nbArg = len (argv)
-if nbArg <2: print (help)
-elif argv[1] == 'help':
-	from funcHelp import printHelp
-	printHelp (imageName)
-elif argv[1] in 'es':
-	image = ImageFile (imageName)
-	image.open()
-	image.title = image.title +" bis"
-	if argv[1] == 'e':
-		imageRef = ImageFile (imageRefName)
-		imageRef.open()
-		image.eraseColors (imageRef)
-	elif argv[1] == 's': image.simplifyColors()
-	image.draw()
-else: print (help)
-
-"""
 if nbArg <3: print (help)
 elif argv[2] == 'help':
 	from funcHelp import printHelp
@@ -48,4 +30,4 @@ elif argv[2] in 'es':
 		image.eraseColors (imageRef)
 	elif argv[2] == 's': image.simplifyColors()
 	image.draw()
-"""
+else: print (help)
