bbf9178894416d3cbd48898778ce12fe324e5665
deborah-powers <deborah.powers.dev@gmail.com>
2026-07-19 12:34:54 +0200

    maison 07/19 12:34

--- calendrier.py
+++ calendrier.py
@@ -26,7 +26,7 @@ les valeurs de tag
 	repas:		"	"	repas
 """ % __file__
 
-dateStart = DatePerso (2026, 6, 20)
+dateStart = DatePerso (2026, 7, 12)
 dateEnd = DatePerso.today()
 dateEndStr = dateEnd.toStrDay()
 """
